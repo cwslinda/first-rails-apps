58d0670d3032572b810b17a1b672c2247c234387
lindacheung <lindacheung94@gmail.com>
2021-11-22 11:46:55 +0800

    insert form

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  get 'about', to: 'pages#about'
+  root to: 'pages#home' # prefix root => root.url + root_path
+  get 'about', to: 'pages#about', as: :about # prefix: about=> about_url + about_path
+  get 'contact', to: 'pages#contact', as: :contact # prefix contact => contact_url + contact_path
 end
 
 # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
